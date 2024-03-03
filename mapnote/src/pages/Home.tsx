0325c6a98353227cbaf536207ce2a567ae88bc74
deltam3 <practicect@proton.me>
2024-03-03 15:48:37 +0900

    deleted outside nodemodules & npm i leaflet correctly

--- mapnote/src/pages/Home.tsx
+++ mapnote/src/pages/Home.tsx
@@ -1,13 +1,23 @@
-import React from "react";
+import { Link } from "react-router-dom";
 
-type Props = {};
-
-const Home = (props: Props) => {
+function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <main>
+      <section>
+        <h1>
+          맵노트
+          <hr />
+          여행간 곳을 기록하세요~
+        </h1>
+        <h2>
+          맵노트를 통해 세계를 기록하세요! 여행 기록, 정리, 회상까지 할 수
+          있습니다. 이 앱 통해 여행 경험을 문서화하여 여행을 잊지 못할 추억으로
+          만드세요.
+        </h2>
+        <Link to="/login">기록하러 가기</Link>
+      </section>
+    </main>
   );
-};
+}
 
 export default Home;
